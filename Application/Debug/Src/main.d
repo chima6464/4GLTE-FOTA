60bc6b7daedaa29d61b516b61f03ef3cfca4433c
Henry Okoro <henry.okoro@georgfischer.com>
2025-01-11 14:30:29 -0600

    First commit

---
+++ Application/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/core_cm4.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_version.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/core_cm4.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_version.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Include/mpu_armv7.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/henry/STM32CubeIDE/workspace_1.14.0/Application/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
